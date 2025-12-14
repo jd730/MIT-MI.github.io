340bd0eb9c1389c60bebe064a29a4a453fd43fc5
tropylium <adg4096@gmail.com>
2025-12-14 15:32:02 -0500

    deploy for github pages

--- astro.config.ts
+++ astro.config.ts
@@ -19,7 +19,8 @@ import { pluginLineNumbers } from '@expressive-code/plugin-line-numbers'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  site: 'https://astro-erudite.vercel.app',
+  site: 'MIT-MI.github.io',
+  base: '/',
   integrations: [
     expressiveCode({
       themes: ['github-light', 'github-dark'],
